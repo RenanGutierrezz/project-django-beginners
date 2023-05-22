dbd74d78c3c60ac7fea46e2031457a8d5a01768d
Renan Almeida Gutierrez <gutierrez.renan73@gmail.com>
2023-05-22 20:38:53 -0300

    name and app_name

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 
 #  eg: domain/sobre 
 urlpatterns = [
-    path('', views.home), # Home
-    path('recipes/<int:id>/',views.recipe), 
+    path('', views.home, name="home"), # Home
+    path('recipes/<int:id>/',views.recipe, name="recipe"), 
 ]
